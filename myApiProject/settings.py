45ad0d39d1e0eedacf06d78c29d6b2451c9f3c12
HuyNguyen1725 <0702huy@gmail.com>
2026-03-26 21:45:06 +0700

    update cors_allowed_origins

--- myApiProject/settings.py
+++ myApiProject/settings.py
@@ -64,7 +64,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://ecommerce-frontend-react-gpuu.vercel.app",
-    "https://www.huynguyen1725.com"
+    "https://huynguyen1725.com",
+    "https://www.huynguyen1725.com",
 ]
 
 ALLOWED_HOSTS = ["*"]
